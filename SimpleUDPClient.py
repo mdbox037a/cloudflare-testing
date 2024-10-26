2b0db8c16ca5640818225e698af9bae25d0ad58e
mdbox037a <128308984+mdbox037a@users.noreply.github.com>
2024-10-26 11:46:19 -0400

    Create SimpleUDPClient.py

---
+++ SimpleUDPClient.py
@@ -0,0 +1,8 @@
+from socket import *
+serverName = input('To whom would you like to connect?:')
+serverPort = input('On which port are they listening?:')
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+message = input('')
+
+
+# in progress - Oct  26 2024 11:46
