45c05f459c8936ee48a4be1638b7bdae913f365d
mdbox037a <128308984+mdbox037a@users.noreply.github.com>
2024-10-26 20:27:58 -0400

    Update SimpleUDPClient.py

--- SimpleUDPClient.py
+++ SimpleUDPClient.py
@@ -2,7 +2,11 @@ from socket import *
 serverName = input('To whom would you like to connect?:')
 serverPort = input('On which port are they listening?:')
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = input('Input loercase sentence:')
+message = input('Input lowercase sentence:')
+clientSocket.sendto(message.encode(),(serverName, serverPort))
+modifiedMessage, serverAddress ` clientSocket.recvfrom(2048)
+print(modifiedMessage.decode())
+clientSocket.close()
 
 
-# in progress - Oct  26 2024 11:52
+# in progress - Oct  26 2024 20:27
